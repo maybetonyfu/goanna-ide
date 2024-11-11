e6070db69cc478138d3322cd1a34128daa27484e
Tony Fu <tonyfu@fastmail.com>
2024-11-11 15:31:39 +1100

    Path

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import App from './App.svelte'
 import { mount } from 'svelte';
-import '@splidejs/svelte-splide/css';
 import './app.css'
 
 const app = mount(App, {target: document.getElementById('app')})
